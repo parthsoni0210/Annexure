39bf8f685cb4d5e9917e927b4ad5ad8e1543bf02
Future3B <future3billionaires.gmail.com>
2020-03-01 15:12:10 +0530

    static files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { AccountSetupComponent } from './account-setup/account-setup.component';
 
 
 const routes: Routes = [
   { path: 'login', component:LoginComponent },
+  { path: 'account-setup', component:AccountSetupComponent }
 ];
 
 @NgModule({
